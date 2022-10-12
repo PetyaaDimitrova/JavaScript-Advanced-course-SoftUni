ac0c657f4aa2b881d77c08d4538868bac61a6063
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-12 13:47:18 +0300

    basic exersises

---
+++ 01_Syntax_Functions_Statements/3_Largest_Number.js
@@ -0,0 +1,10 @@
+function maxNumber(a,b,c){
+    if(a >= b & a >= c){
+        console.log(a);
+    } else if(b > a & b > c){
+        console.log(b);
+    }else {
+        console.log(c);
+    }
+}
+maxNumber(-3, -5, -22.5);
---
+++ 01_Syntax_Functions_Statements/5_Math_Operations.js
@@ -0,0 +1,9 @@
+function solve (a, b , operator){
+    if(operator == '*'){
+        console.log(a * b);
+    } else if(operator== '/'){
+        console.log(a/b);
+    }
+}
+
+solve(3 , 4 , '/');
