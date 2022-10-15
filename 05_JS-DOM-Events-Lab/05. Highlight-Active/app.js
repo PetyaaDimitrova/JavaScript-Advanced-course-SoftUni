008a8fb1ede0abf93bada5812d543a63573c57a2
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-15 15:54:27 +0300

    DOM events fundamentals

--- 05_JS-DOM-Events-Lab/05. Highlight-Active/app.js
+++ 05_JS-DOM-Events-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+   let inputs = document.getElementsByTagName("input");
+
+   for(let input of inputs){
+    input.addEventListener("focus", inputFocus);
+    input.addEventListener("blur", inputBlur);
+   }
+
+   function inputFocus(event){
+    event.target.parentElement.classList.add("focused")
+   }
+   function inputBlur(event){
+    event.target.parentElement.classList.remove("focused")
+   }
 }
