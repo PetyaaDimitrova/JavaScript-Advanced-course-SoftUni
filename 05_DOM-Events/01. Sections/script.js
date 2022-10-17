8a15251cb50be9b1705139a7f210cc9595e01d3b
Petya Dimitrova <101112384+PetyaaDimitrova@users.noreply.github.com>
2022-10-17 17:06:35 +0300

    Events

--- 05_DOM-Events/01. Sections/script.js
+++ 05_DOM-Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   for(let element of words){
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      p.innerText = element;
+      div.appendChild(p);
+      p.style.display = "none";
+      div.addEventListener("click", onClick);
+      document.getElementById("content").appendChild(div);
+
+   }
+
+   function onClick(event){
+let p = event.target.children[0];
+p.style.display = "block";
+   }
 }
--- 05_DOM-Events/02. Time-Converter/solution.js
+++ 05_DOM-Events/02. Time-Converter/solution.js
@@ -1,4 +1,41 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll("input[type=button]"));
+
+    for (let button of buttons) {
+        button.addEventListener("click", convert);
+    }
+
+    function convert(event) {
+        let value = event.target.parentElement.querySelector("input[type=text]").value;
+        let unit = event.target.id;
+
+        switch (unit) {
+            case "daysBtn":
+                populate(value)
+                break;
+            case "hoursBtn":
+                populate(value / 24)
+                break;
+            case "minutesBtn":
+                populate(value / 24 / 60)
+                break;
+            case "secondsBtn":
+                populate(value / 24 / 60 / 60)
+                break;
+        }
+
+    }
+    function populate(value) {
+
+        let inputs = Array.from(document.querySelectorAll("input[type=text]"));
+        inputs.shift().value = value;
+        let currentValue = value * 24;
+        for(let el of inputs){
+            el.value = currentValue;
+            currentValue = currentValue * 60;
+        }
+
+
+    }
 }
